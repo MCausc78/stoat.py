b8f6b2d65e24fe40548872c63ce648beac54ef9a
stranger <128334043+jinbao4@users.noreply.github.com>
2026-05-03 17:39:50 +0100

    Add `avatar_url` to `DisplayUser` (#42)

--- stoat/user.py
+++ stoat/user.py
@@ -335,7 +335,7 @@ class BaseUser(Base, Connectable, Messageable):
     def default_avatar_url(self) -> str:
         """:class:`str`: The URL to user's default avatar."""
         return self.state.http.url_for(routes.USERS_GET_DEFAULT_AVATAR.compile(user_id=self.id))
-
+        
     @property
     def dm_channel_id(self) -> typing.Optional[str]:
         """Optional[:class:`str`]: The ID of the private channel with this user."""
@@ -1346,6 +1346,14 @@ class DisplayUser(BaseUser):
     def avatar(self) -> typing.Optional[Asset]:
         """Optional[:class:`Asset`]: The avatar of the user."""
         return self.internal_avatar and self.internal_avatar.attach_state(self.state, 'avatars')
+        
+    @property
+    def display_avatar_url(self) -> str:
+        """:class:`str`: The URL to user's avatar, or their default avatar if they have none set."""
+        avatar = self.internal_avatar
+        if avatar is None:
+            return self.default_avatar_url
+        return self.state.cdn_client.url_for(avatar.id, 'avatars')
 
     @property
     def tag(self) -> str:
